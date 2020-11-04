a43ca9efa60bdbb8f1d35fdb8520b0ac98b05f8e
Salome Emrot <salome.emrot@gmail.com>
2020-11-04 16:44:49 +0400

    end of day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning The DB"
+Ingredient.destroy_all
+
+puts "Creating ingredients"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "sugar syrup")
+Ingredient.create(name: "egg")
+Ingredient.create(name: " peach nectar")
+Ingredient.create(name: "coconut milk")
+Ingredient.create(name: "vanilla syrup")
+Ingredient.create(name: "pineapple")
+Ingredient.create(name: "ginger")
